1bec385af794768001830d9390fdbd860655068c
yugecin <yugecin@users.noreply.github.com>
2020-11-16 21:15:23 +0100

    Spectrum on harrier

--- zharrier.cs
+++ zharrier.cs
@@ -9,6 +9,10 @@ partial class all{
 	public static harrupdate haha;
 	public static vec3 sunpos = v3(0f); // see zcamera
 	class Zharrier : Z {
+
+		const int SPECTRUM_HEIGHT = 10;
+		const int SPECTRUM_WIDTH = 15;
+
 		/*
 		public static vec3 position;
 		public static float pitch;
@@ -24,6 +28,9 @@ partial class all{
 		Odot[] dots;
 		Pixelscreen pixelscreen = new Pixelscreen(/*widescreen mode*/854/2, 480/2, 2);
 		public static Odot dot;
+
+		Odot[][] spectrumdots;
+		Odot[][] spectrumdots2;
 
 		public Zharrier(int start, int stop) {
 
@@ -63,9 +70,20 @@ partial class all{
 					), s);
 			}
 			move(points, v3(15f, 0f, 0f));
+			spectrumdots = new Odot[SPECTRUM_HEIGHT][];
+			spectrumdots2 = new Odot[SPECTRUM_HEIGHT][];
+			for (int i = 0; i < SPECTRUM_HEIGHT; i++) {
+				spectrumdots[i] = new Odot[SPECTRUM_WIDTH];
+				spectrumdots2[i] = new Odot[SPECTRUM_WIDTH];
+				for (int j = 0; j < SPECTRUM_WIDTH; j++) {
+					spectrumdots[i][j] = new Odot("", 0);
+					spectrumdots2[i][j] = new Odot("", 0);
+				}
+			}
 		}
 
-		public override void draw(SCENE scene) {
+		public override void draw(SCENE scene)
+		{
 			ICommand.round_move_decimals.Push(DECIMALS_PRECISE);
 			ICommand.round_scale_decimals.Push(DECIMALS_PRECISE);
 			ICommand.round_rot_decimals.Push(DECIMALS_PRECISE);
@@ -76,10 +94,10 @@ partial class all{
 			}
 
 			copy(_points, points);
-			turn(_points, v3(0f), quat(pitch2.valueAt(scene.time) * -10f, 0f, 0f));
-			turn(_points, v3(0f), quat(0f, roll.valueAt(scene.time) * 10f, 0f));
-			turn(_points, v3(0f), quat(pitch.valueAt(scene.time) * -10f, 0f, 0f));
-			turn(_points, v3(0f), quat(0f, 0f, yaw.valueAt(scene.time) * 10f));
+			turn(_points, v3(0f), quat(pitch2.valueAt(scene.time) * -2f, 0f, 0f));
+			turn(_points, v3(0f), quat(0f, roll.valueAt(scene.time) * 5f, 0f));
+			turn(_points, v3(0f), quat(pitch.valueAt(scene.time) * -5f, 0f, 0f));
+			turn(_points, v3(0f), quat(0f, 0f, yaw.valueAt(scene.time) * 5f));
 			move(_points, getHarrPos(scene.time));
 			/*
 			move(_points, position);
@@ -127,11 +145,11 @@ partial class all{
 			System.Drawing.Font font = new System.Drawing.Font("Tahoma", 12.0f);
 			if (!rendering) {
 				for (int i = 0; i < harr.points.Length; i++) {
-					vec3 p = harr.points[i];
-					vec3[] _p = new vec3[] { p };
-					move(_p, Zcamera.mid);
-					Zcamera.adjust(_p);
-					vec4 px = project(_p[0]);
+					//vec3 p = harr.points[i];
+					//vec3[] _p = new vec3[] { p };
+					//move(_p, Zcamera.mid);
+					//Zcamera.adjust(_p);
+					vec4 px = project(_points[i]);
 					if (udata[6] > 0 && px.w > 0) {
 						scene.g.DrawString(i.ToString(), font, new SolidBrush(Color.White), px.x + 1, px.y + 1);
 						scene.g.DrawString(i.ToString(), font, new SolidBrush(Color.Blue), px.x, px.y);
@@ -139,8 +157,46 @@ partial class all{
 				}
 			}
 
+			if (28000 < scene.time && scene.time < 31458) {
+				float[] fftval = fft.SmoothValue(scene.time).values;
+				for (int i = 0; i < SPECTRUM_HEIGHT; i++) {
+					float yval = (i + 1) / (float) (SPECTRUM_HEIGHT + 1);
+					float y = i / (float)(SPECTRUM_HEIGHT - 1) * 0.8f + 0.1f;
+					float r = clamp(yval * 2f, 0f, 1f);
+					float g = 1f - clamp((yval - .5f) * 2f, 0f, 1f);
+					vec4 col = v4(r, g, 0f, 1f);
+					for (int j = 0; j < SPECTRUM_WIDTH; j++) {
+						float x = j / (float)(SPECTRUM_WIDTH - 1) * 0.9f + 0.05f;
+						float fftvalx = eq_out_circ(fftval[j]);//clamp(0f, 1f, fftval[j] * 10f);
+						if (!tris[22].tri.shouldcull() && yval <= fftvalx) {
+							vec3 a = lerp(_points[80], _points[81], y);
+							vec3 b = lerp(_points[79], _points[78], y);
+							vec4 p = project(lerp(a, b, x));
+							spectrumdots[i][j].update(scene.time, col, p);
+							spectrumdots[i][j].draw(scene.g);
+						} else {
+							spectrumdots[i][j].update(scene.time, col, null);
+						}
+						if (!tris[14].tri.shouldcull() && yval <= fftvalx) {
+							vec3 a = lerp(_points[77], _points[76], y);
+							vec3 b = lerp(_points[75], _points[74], y);
+							vec4 p = project(lerp(a, b, x));
+							spectrumdots[i][j].update(scene.time, col, p);
+							spectrumdots[i][j].draw(scene.g);
+						} else {
+							spectrumdots[i][j].update(scene.time, col, null);
+						}
+					}
+				}
+			}
+
 			if (!rendering) {
 				vec3[] v2 = new vec3[] { harrpoint };
+				turn(v2, v3(0f), quat(pitch2.valueAt(scene.time) * -2f, 0f, 0f));
+				turn(v2, v3(0f), quat(0f, roll.valueAt(scene.time) * 5f, 0f));
+				turn(v2, v3(0f), quat(pitch.valueAt(scene.time) * -5f, 0f, 0f));
+				turn(v2, v3(0f), quat(0f, 0f, yaw.valueAt(scene.time) * 5f));
+				move(v2, getHarrPos(scene.time));
 				move(v2, Zcamera.mid);
 				Zcamera.adjust(v2);
 				dot.update(scene.time, v4(1f, 0, 0, 1f), project(v2[0]));
@@ -162,6 +218,16 @@ partial class all{
 			}
 			foreach (Otri2 t in tris) {
 				t.fin(w);
+			}
+			foreach (Odot[] dots in spectrumdots) {
+				foreach (Odot dot in dots) {
+					dot.fin(w);
+				}
+			}
+			foreach (Odot[] dots in spectrumdots2) {
+				foreach (Odot dot in dots) {
+					dot.fin(w);
+				}
 			}
 			ICommand.round_move_decimals.Pop();
 			ICommand.round_scale_decimals.Pop();
